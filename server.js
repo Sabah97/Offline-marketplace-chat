5747099739bc9eaea33cfb9521b4cd3195abe122
Sabah97 <sabah.ashraf97@gmail.com>
2019-11-27 00:24:10 +0600

    assignment2

---
+++ server.js
@@ -0,0 +1,41 @@
+
+var http = require("http");
+const express = require("express");
+const app = express();
+const server = http.Server(app);
+
+var io = require('socket.io')(server);
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./view'))
+
+app.get('/home', function(req, res){
+    res.sendFile(__dirname + '/cart.html');
+  });
+
+ io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+  server.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
