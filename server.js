8839e695d35a77f296cf9a1fbe245b6527d572e5
Sabah97 <sabah.ashraf97@gmail.com>
2019-11-27 08:33:21 +0600

    assignment2

--- server.js
+++ server.js
@@ -13,29 +13,31 @@ app.get('/home', function(req, res){
     res.sendFile(__dirname + '/cart.html');
   });
 
- io.on('connection', function(socket){
-  console.log('a user connected');
+//  io.on('connection', function(socket){
+//   console.log('a user connected');
+
+// });
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message',msg);
+    console.log('message: ' + msg);
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+// io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+// io.on('connection', function(socket){
+//   socket.broadcast.emit('hi');
+// });
 
-io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
   server.listen(3000, function(){
     console.log('listening on *:3000');
   });
